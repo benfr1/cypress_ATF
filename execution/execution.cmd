2659060681f3d8b0deaa7c6967b4a9d86b00e555
DG6166 <DG6166@engie.com>
2021-11-19 08:08:25 +0700

    save

--- execution/execution.cmd
+++ execution/execution.cmd
@@ -1,4 +1,4 @@
 REM C:\hiboo_yarn_cypress\third-party\cypress\Cypress\Cypress.exe cypress run --browser chrome --headed --no-exit --reporter junit --reporter-options mochaFile=result.xml,toConsole=true --spec "I can display home page succesffully.js"
 
 cd "C:\hiboo_yarn_cypress\third-party\cypress\Cypress"
-cypress run --spec "C:\hiboo_yarn_cypress\cypress\integration\I can display home page succesffully.js" --project "C:\hiboo_yarn_cypress" --global --headed --browser chrome
+cypress run --spec "C:\hiboo_yarn_cypress\I can display home page succesffully.js" --project "C:\hiboo_yarn_cypress" --global --headed --browser chrome
